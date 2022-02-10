5e6f682d66b053be5350d573c8bfa03cea638a1c
Attila Neto <netovisk10@hotmail.com>
2022-02-10 09:55:37 -0300

    Adicionado Lotofacil; Adicionado Quina; Simplificação da Função de sorteio

--- script.js
+++ script.js
@@ -1,9 +1,48 @@
-function SorteioMegasena(){
+
+
+
+function ChamarMegaSena(){
+    divjogo = document.getElementById('jogo')
+    
+    codigo = "<img class='logoimg' src='/img/botao-mega-sena.png'><br><label>Quantidade de Numeros Sorteados: </label><select id='qntNum'><option selected value=6>6</option><option value=7>7</option><option value=8>8</option><option value=9>9</option><option value=10>10</option><option value=11>11</option><option value=12>12</option><option value=13>13</option><option value=14>14</option><option value=15>15</option></select><label>Total de Jogos Sorteados: </label><input id='qntJogos' type='number' value=1><br><br><button onclick='Sorteio(60)'>Sortear!</button>"
+
+    divjogo.innerHTML = codigo
+}
+
+function ChamarLotofacil(){
+    divjogo = document.getElementById('jogo')
+
+    codigo = "<img class='logoimg' src='/img/botao-lotofacil.png'><br><label>Quantidade de Numeros Sorteados: </label><select id='qntNum'><option value=15>15</option><option value=16>16</option><option value=17>17</option><option value=18>18</option><option value=19>19</option><option value=20>20</option></select><label>Quantidade de Jogos: </label><input id='qntJogos' type='number' value=1><br><br><button onclick='Sorteio(25)'>Sortear!</button>"
+
+    divjogo.innerHTML = codigo
+}
+
+function ChamarQuina(){
+
+    divjogo = document.getElementById('jogo')
+
+    codigo = "<img class='logoimg' src='/img/botao-quina.png'><br><label>Quantidade de Numeros Sorteados: </label><select id='qntNum'><option selected value=5>5</option><option value=6>6</option><option value=7>7</option><option value=8>8</option><option value=9>9</option><option value=10>10</option><option value=11>11</option><option value=12>12</option><option value=13>13</option><option value=14>14</option><option value=15>15</option></select><label>Total de Jogos Sorteados: </label><input id='qntJogos' type='number' value=1><br><br><button onclick='Sorteio(80)'>Sortear!</button>"
+
+    divjogo.innerHTML = codigo
+}
+
+
+
+
+function Sorteio(totalSorteio){
     qntNum = document.getElementById('qntNum').value;
     qntJogos = document.getElementById('qntJogos').value;
-    resultado = document.getElementById('resultado')
+           
+    SortearNumeros(qntJogos, qntNum, totalSorteio)
+}
 
-    numeroSorteado = Math.floor(Math.random()*(60+1)+1)
+function ChecarNumero(a,b){
+    return (a - b)
+}
+
+function SortearNumeros(qntJogos, qntNum, numSort){
+
+    resultado = document.getElementById('resultado')
 
     jogosSorteados = []
 
@@ -16,7 +55,7 @@ function SorteioMegasena(){
         for (let i = 1; i <= qntNum; i++) {              
 
             while (numerosSorteados.length < qntNum){
-                var numero = numeroSorteado = Math.floor(Math.random()*(60+1)+1)
+                var numero = numeroSorteado = Math.floor(Math.random()*(numSort+1)+1)
                 numero = ("00" + numero).slice(-2)
                 if (numerosSorteados.indexOf(numero) == -1){
                     numerosSorteados.push(numero)
@@ -36,7 +75,7 @@ function SorteioMegasena(){
                  
             }
         }
-        jogosSorteados.push(jogo)       
+        jogosSorteados.push(jogo)
     }
 
     codigo = ''
@@ -47,9 +86,4 @@ function SorteioMegasena(){
     }
     
     resultado.innerHTML = codigo
-    
-}
-
-function ChecarNumero(a,b){
-    return (a - b)
 }
